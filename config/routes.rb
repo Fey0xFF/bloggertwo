50091f8aad30992e32177cc6199e2a6fcdbfcc44
Anthony Bendas <anthony.bendas@gmail.com>
2017-10-25 16:44:14 -0600

    Sorcery authentication complete, left out for commit for free access

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :authors
   root to: 'articles#index'
   resources :tags
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
 end
